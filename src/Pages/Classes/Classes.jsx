a0b085aa593d71478335b72409f8d98e0605e882
riyad3399 <programminghero.riyad@gmail.com>
2023-06-12 01:04:58 +0600

    added the enrolled routes

--- src/Pages/Classes/Classes.jsx
+++ src/Pages/Classes/Classes.jsx
@@ -13,19 +13,24 @@ const Classes = () => {
       });
   }, []);
 
-  const datas = classData.filter(data => data.status === "accept")
+  const datas = classData.filter((data) => data.status === "accept");
 
   return (
-    <div className="pt-12 pb-5">
+    <div className=" pb-5">
       <Helmet>
         <title>Classes - Sport Spark</title>
       </Helmet>
-     
-      <h3 className="text-center font-bold text-4xl my-5">Total Classes: { datas.length}</h3>
+      <div className="pb-6">
+      <RoutesTitel
+        subHeading={"All Classes List"}
+        heading={"all classes"}
+      ></RoutesTitel>
+      </div>
+    
       <div className="grid md:grid-cols-2 gap-6">
-      {datas.map((data) => (
-        <ClassesCard key={data._id} data={data}></ClassesCard>
-      ))}
+        {datas.map((data) => (
+          <ClassesCard key={data._id} data={data}></ClassesCard>
+        ))}
       </div>
     </div>
   );
